28ae78c8db8033ee8df9e2f43409d8e5c4282279
toritomodachi <tancheonn@gmail.com>
2023-10-27 15:47:03 +0900

    1page 완성

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function change() {
+    var ptag = document.getElementById("drop");
+    
+    if(ptag.style.display !== "none"){
+        ptag.style.display = "none";
+    }
+    else{
+        ptag.style.display="block";
+    }
+}
